8e7421cd88a1992546f3443910fc5817bd3642bf
Afonso-Mp7 <shark11927@sharkcoders.pt>
2026-06-29 20:16:23 +0100

    Add files via upload

---
+++ Aula 17.py
@@ -0,0 +1,4 @@
+with open('file_with_three_lines.txt', 'w') as file2:
+    file2.write('Primeira linha de informação\n')
+    file2.write('Segunda linha de informação\n')
+    file2.write('Terceira linha de informação\n')
